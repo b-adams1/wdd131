cfd2538b8cb9c706f16f9f74b2cfbb96635a476a
b-adams1 <bmanx7@gmail.com>
2025-10-15 21:29:37 -0600

    yada

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -4,6 +4,7 @@ function toggleMenu() {
     menu.classList.toggle("hide");
 }
 menuButton.addEventListener("click", toggleMenu);
+
 function handleResize() {
     const menu = document.querySelector(".menu");
     if (window.innerWidth > 1000) {
@@ -13,5 +14,31 @@ function handleResize() {
         menu.classList.add("hide");
     }
 }
+
 handleResize();
 window.addEventListener("resize", handleResize);
+
+const newDialog = document.createElement("dialog");
+newDialog.innerHTML = "<img><button class='close_viewer'>X</button>";
+document.body.appendChild(newDialog);
+const newCss = document.createElement("style");
+newCss.innerHTML = `
+img {
+    width: 100%;
+    max-width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, .6);
+    margin: 20vh auto; }
+.close_viewer {
+    position: absolute;
+    top: 20vh;
+    max-height: 100%;
+    z-index: 10;}`
+
+const gallery = document.querySelector('.gallery');
+const modal = document.querySelector('dialog');
+const modalImg = document.querySelector('img');
+const closeButton = document.querySelector('.close_viewer');
+function thingy(event) {
+    event.target.closest('img')
+}
