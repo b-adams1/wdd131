b4ef7182a5299b66ca129993b4d205030d9921b8
b-adams1 <bmanx7@gmail.com>
2025-10-23 15:36:10 -0600

    homeworks

---
+++ ponder/arrays.js
@@ -0,0 +1,31 @@
+const steps = ["one", "two", "three"];
+function listTemplate(step) {
+  return `<li>${step}</li>`; //the html string made from step
+}
+const stepsHtml = steps.map(listTemplate);// use map to convert the list from strings to HTML
+document.querySelector("#myList").innerHTML = stepsHtml.join("")// set the innerHTML
+
+const letters = ["A", "B", "A"];
+function getGpa(letter) {
+    let points = 0;
+    if (grade === "A") {
+        points = 4;
+    } else if (grade === "B") {
+        points = 3;
+    }
+    return points;
+}
+const gpaPoints = grades.map(getGpa);
+const totalPoints = gpaPoints.reduce(function (total, item) {
+    return total + item;
+});
+const gpa = totalPoints / gpaPoints.length;
+
+const words = ["watermelon", "peach", "apple", "tomato", "grape"];
+const shortWords = words.filter(function (word) {
+    return word.length < 6;
+});
+
+const myArray = [12, 34, 21, 54];
+const luckyNumber = 21;
+let luckyIndex = myArray.indexOf(luckyNumber);
